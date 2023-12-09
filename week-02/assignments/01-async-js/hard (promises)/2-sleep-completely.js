f23d2ebd3c1109fbf4b40aa8282812193c855358
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-09 13:24:27 -0800

    solves sleep completely problem

--- week-02/assignments/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-02/assignments/01-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,12 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+    const start = new Date().getTime()
+    while (new Date().getTime() - start < seconds) {}
 }
+
+console.log('Start')
+sleep(3000)
+console.log('After 3 seconds')
+console.log('End')

44644f8437b2c75d31a935d0efd3f403c17627a7
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-13 19:38:02 -0800

    solves todoServer problem using in memory data

--- week-02/assignments/02-nodejs/todoServer.js
+++ week-02/assignments/02-nodejs/todoServer.js
@@ -43,7 +43,64 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+const PORT = 3000
 
 app.use(bodyParser.json())
+
+let todos = []
+
+app.get('/todos', (req, res) => {
+    res.status(200).json({ todos })
+})
+
+app.get('/todos/:id', (req, res) => {
+    const todoId = parseInt(req.params.id)
+    const todo = todos.find((todo) => todo.id === todoId)
+
+    if (todo) {
+        res.status(200).json({ todo })
+    } else {
+        res.status(404).json({ error: 'Not found.' })
+    }
+})
+
+app.post('/todos', (req, res) => {
+    const newTodo = req.body
+    const newTodoId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1
+    newTodo.id = newTodoId
+    todos.push(newTodo)
+
+    res.status(201).json({ id: newTodoId })
+})
+
+app.put('/todos/:id', (req, res) => {
+    const todoId = parseInt(req.params.id)
+    const todoIndex = todos.findIndex((todo) => todo.id === todoId)
+
+    if (todoIndex !== -1) {
+        todos[todoIndex] = { ...todos[todoIndex], ...req.body }
+
+        res.status(200).json({ data: 'Todo successfully updated.' })
+    } else {
+        res.status(404).json({ error: 'Todo not found.' })
+    }
+})
+
+app.delete('/todos/:id', (req, res) => {
+    const todoId = parseInt(req.params.id)
+    const todoIndex = todos.findIndex((todo) => todo.id === todoId)
+
+    if (todoIndex !== -1) {
+        todos.splice(todoIndex, 1)
+
+        res.status(200).json({ message: 'Todo successfully deleted.' })
+    } else {
+        res.status(404).json({ error: 'Todo not found.' })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+})
 
 module.exports = app

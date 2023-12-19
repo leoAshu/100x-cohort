1bab9891bc1db4ecbece39d14531b2af3e12ffce
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-19 14:28:10 -0800

    defines the models for week 3 mongo assignment

--- week-03/assignments/03-mongo/db/index.js
+++ week-03/assignments/03-mongo/db/index.js
@@ -1,27 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://ashutoshojha2009:123412312@cluster0.tqadczh.mongodb.net/100xCohort')
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+})
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+})
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    id: { type: Number, required: true },
+    title: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    imageLink: { type: String },
+    published: { type: Boolean },
+})
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model('Admin', AdminSchema)
+const User = mongoose.model('User', UserSchema)
+const Course = mongoose.model('Course', CourseSchema)
 
 module.exports = {
     Admin,
     User,
-    Course
+    Course,
 }

ecdf8dcddc241fe633b487db3f30064bf0837bb9
leoAshu <ashutosh.ojha2009@gmail.com>
2023-12-22 15:28:01 -0800

    refactors admin routers

--- week-03/assignments/03-mongo/routes/admin.js
+++ week-03/assignments/03-mongo/routes/admin.js
@@ -5,7 +5,7 @@ const generateUniqueId = require('../utils')
 const router = Router()
 
 // Admin Routes
-router.route('/signup').post(async (req, res) => {
+router.post('/signup', async (req, res) => {
     const { username, password } = req.body
 
     const adminExists = await Admin.findOne({ username })
@@ -23,9 +23,15 @@ router.route('/signup').post(async (req, res) => {
     res.status(200).json({ msg: 'Admin signup successful' })
 })
 
-router.route('/courses').post(adminMiddleware, async (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     const username = req.headers.username
     const { title, description, price, image } = req.body
+
+    const courseExists = await Course.findOne({ title, description, price })
+    if (courseExists) {
+        res.status(400).json({ err: 'Course already exists' })
+        return
+    }
 
     const course = new Course({
         id: generateUniqueId(),
@@ -40,7 +46,7 @@ router.route('/courses').post(adminMiddleware, async (req, res) => {
     res.status(200).json({ msg: 'Course creation successful' })
 })
 
-router.route('/courses').get(adminMiddleware, async (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     const username = req.headers.username
 
     const courses = await Course.find({ createdBy: username })

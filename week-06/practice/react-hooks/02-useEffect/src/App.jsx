6cec5b4cf8ae48240a201221fa3f2ffd7c01ee6a
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-08 17:42:24 -0800

    week-6: useEffect practice code

--- week-06/practice/react-hooks/02-useEffect/src/App.jsx
+++ week-06/practice/react-hooks/02-useEffect/src/App.jsx
@@ -2,28 +2,33 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
-    const [todos, setTodos] = useState([])
-
-    useEffect(() => {
-        axios.get('https://sum-server.100xdevs.com/todos').then(function (response) {
-            setTodos(response.data.todos)
-        })
-    }, [])
+    const [selectedId, setSelectedId] = useState(1)
 
     return (
-        <>
-            {todos.map((todo) => (
-                <Todo key={todo.id} title={todo.title} description={todo.description} />
-            ))}
-        </>
+        <div>
+            <button onClick={() => setSelectedId(1)}>1</button>
+            <button onClick={() => setSelectedId(2)}>2</button>
+            <button onClick={() => setSelectedId(3)}>3</button>
+            <button onClick={() => setSelectedId(4)}>4</button>
+            <button onClick={() => setSelectedId(5)}>5</button>
+            <Todo id={selectedId} />
+        </div>
     )
 }
 
-function Todo({ title, description }) {
+function Todo({ id }) {
+    const [todo, setTodo] = useState({})
+
+    useEffect(() => {
+        axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).then(function (response) {
+            setTodo(response.data.todo)
+        })
+    }, [id])
+
     return (
         <div>
-            <h4>{title}</h4>
-            <p>{description}</p>
+            <h4>{todo.title}</h4>
+            <p>{todo.description}</p>
         </div>
     )
 }

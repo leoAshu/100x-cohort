a6a4cba183ccb1365f74948b6ee3c7663da98139
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-08 17:43:47 -0800

    week-6: useState practice code

--- week-06/practice/react-hooks/01-useState/src/App.jsx
+++ week-06/practice/react-hooks/01-useState/src/App.jsx
@@ -1,37 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
-    const [todos, setTodos] = useState([])
-
-    useEffect(() => {
-        async function getData() {
-            const response = await fetch('https://sum-server.100xdevs.com/todos')
-            const data = await response.json()
-            console.log(data)
-            setTodos(data.todos)
-        }
-
-        getData()
-
-        setInterval(() => {
-            getData()
-        }, 10000)
-    }, [])
+    const [count, setCount] = useState(0)
 
     return (
-        <>
-            {todos.map((todo) => (
-                <Todo key={todo.id} title={todo.title} description={todo.description} />
-            ))}
-        </>
-    )
-}
-
-function Todo({ title, description }) {
-    return (
-        <div style={{ padding: 4 }}>
-            <h4>{title}</h4>
-            <h5>{description}</h5>
+        <div>
+            <button onClick={() => setCount(count + 1)}>Click Me: {count}</button>
         </div>
     )
 }

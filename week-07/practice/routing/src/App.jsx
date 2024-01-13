89c4562b78bf68057858e252671c55f5e432796c
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-13 06:15:58 -0800

    practice code for routing

--- week-07/practice/routing/src/App.jsx
+++ week-07/practice/routing/src/App.jsx
@@ -1,5 +1,43 @@
+import { BrowserRouter, Link, Route, Routes, useNavigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Landing from './pages/Landing'
+
 function App() {
-    return <></>
+    return (
+        <>
+            <BrowserRouter>
+                <Appbar />
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/" element={<Landing />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+function Appbar() {
+    const navigate = useNavigate()
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    // window.location.href = '/'
+                    navigate('/')
+                }}
+            >
+                Landing Page
+            </button>
+            <button
+                onClick={() => {
+                    // window.location.href = '/dashboard'
+                    navigate('/dashboard')
+                }}
+            >
+                Dashboard Page
+            </button>
+        </div>
+    )
 }
 
 export default App

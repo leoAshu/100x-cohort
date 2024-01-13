052bc145a6787aeee1398508dea70db886b27bce
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-12 17:05:53 -0800

    week-5: assignment solution

---
+++ week-05/assignments/level-1/business-card/src/App.jsx
@@ -0,0 +1,15 @@
+import { BusinessCard } from './BusinessCard'
+
+function App() {
+    return (
+        <BusinessCard
+            name={'Ashutosh'}
+            description={'Aspring Frontend Dev'}
+            interests={['React', 'Node', 'Spring Boot']}
+            linkedin={'www.linkedin.com/in/ojha-ashutosh/'}
+            twitter={'www.linkedin.com/in/ojha-ashutosh/'}
+        />
+    )
+}
+
+export default App

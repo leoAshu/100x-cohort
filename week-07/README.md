195541cddffb65fe5f8ece79c3f31ea47339a7df
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-15 17:28:46 -0800

    week-7.2: initial code for recoil

--- week-07/README.md
+++ week-07/README.md
@@ -15,6 +15,16 @@
 
 ## Prop Drilling
 
+-   repeated unnecessary passing down of state via props to child components
+-   state manages by a parent component is required by a child component deep down in the component tree
+-   needs unncessarily passing down the state via props through all the components in the path until the specific child component
+
 ## Context API
 
--   helps avoid prop drilling
+-   helps avoid prop drilling by teleporting the state directly to the child components that require i
+-   involves three steps
+    -   create the context for the state `createContext()`
+    -   wrap the components (just the root) with `<CustomContext.Provider>` context provider
+    -   call the state context using the `useContext(CustomContext)` hook
+-   only provides syntactical sugar to avoid passing state through props
+-   does not fix the unnecessary re-renders
---
+++ week-07/practice/recoil/src/App.jsx
@@ -0,0 +1,51 @@
+import { useContext, useState } from 'react'
+import { CountContext } from './context'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <CountContext.Provider value={count}>
+            <Count setCount={setCount} />
+        </CountContext.Provider>
+    )
+}
+
+function Count({ setCount }) {
+    return (
+        <>
+            <CountRenderer />
+            <Buttons setCount={setCount} />
+        </>
+    )
+}
+
+function CountRenderer() {
+    const count = useContext(CountContext)
+    return <div>{count}</div>
+}
+
+function Buttons({ setCount }) {
+    const count = useContext(CountContext)
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setCount(count + 1)
+                }}
+            >
+                Increment
+            </button>
+
+            <button
+                onClick={() => {
+                    setCount(count - 1)
+                }}
+            >
+                Decrement
+            </button>
+        </div>
+    )
+}
+
+export default App

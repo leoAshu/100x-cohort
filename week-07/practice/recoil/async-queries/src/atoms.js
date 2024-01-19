27fa73cbafd6d5a218fd507c56f96ba733c4ae1a
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-18 16:41:04 -0800

    week-7: recoil async queries

--- week-07/practice/recoil/async-queries/src/atoms.js
+++ week-07/practice/recoil/async-queries/src/atoms.js
@@ -1,22 +1,26 @@
-import { atom, selector } from "recoil";
+import { atom, selector } from 'recoil'
+import axios from 'axios'
 
 export const notifications = atom({
-    key: "networkAtom",
-    default: {
-        network: 4, 
-        jobs: 6, 
-        messaging: 3, 
-        notifications: 3
-    }
-});
+    key: 'networkAtom',
+    default: selector({
+        key: 'networkAtomSelector',
+        get: async () => {
+            const res = await axios.get('https://sum-server.100xdevs.com/notifications')
+            return res.data
+        }
+    })
+})
 
 export const totalNotificationSelector = selector({
-    key: "totalNotificationSelector",
-    get: ({get}) => {
-        const allNotifications = get(notifications);
-        return allNotifications.network + 
-        allNotifications.jobs + 
-        allNotifications.notifications + 
-        allNotifications.messaging
+    key: 'totalNotificationSelector',
+    get: ({ get }) => {
+        const allNotifications = get(notifications)
+        return (
+            allNotifications.network +
+            allNotifications.jobs +
+            allNotifications.notifications +
+            allNotifications.messaging
+        )
     }
 })

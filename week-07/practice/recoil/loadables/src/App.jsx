35633a15786407b38402fd95caaebbc058608f38
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-18 17:36:19 -0800

    week-7: recoil loadables practice code

--- week-07/practice/recoil/loadables/src/App.jsx
+++ week-07/practice/recoil/loadables/src/App.jsx
@@ -1,28 +1,31 @@
-
 import './App.css'
-import { RecoilRoot, useRecoilStateLoadable } from 'recoil';
-import { todosAtomFamily } from './atoms';
+import { RecoilRoot, useRecoilStateLoadable } from 'recoil'
+import { todosAtomFamily } from './atoms'
 
 function App() {
-  return <RecoilRoot>
-    <Todo id={1}/>
-    <Todo id={2} />
-  </RecoilRoot>
+    return (
+        <RecoilRoot>
+            <Todo id={1} />
+            <Todo id={2} />
+        </RecoilRoot>
+    )
 }
 
-function Todo({id}) {
-   const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
-   if (todo.state === "loading") {
-      return <div>loading</div>
-   }
-   
-   return (
-    <>
-      {todo.contents.title}
-      {todo.contents.description}
-      <br />
-    </>
-  )
+function Todo({ id }) {
+    const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id))
+    if (todo.state === 'loading') {
+        return <div>loading</div>
+    }
+
+    return (
+        <>
+            {todo.contents.title}
+            <br />
+            {todo.contents.description}
+            <br />
+            <br />
+        </>
+    )
 }
 
 export default App

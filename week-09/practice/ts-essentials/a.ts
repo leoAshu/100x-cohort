fe02d0dc6cf48ea84a41c9f053c1b3bd8b4160fb
leoAshu <ashutosh.ojha2009@gmail.com>
2024-01-31 17:05:26 -0800

    week-09: ts-essentials code

--- week-09/practice/ts-essentials/a.ts
+++ week-09/practice/ts-essentials/a.ts
@@ -1,7 +1,22 @@
 // const x: number = 1
 // console.log(x)
 
-const hello = (name: string) => {
-    console.log(`Hello ${name}`)
+// const hello = (name: string) => {
+//     console.log(`Hello ${name}`)
+// }
+// hello('Ashutosh')
+
+const sum = (a: number, b: number): number => {
+    return a + b
 }
-hello('Ashutosh')
+console.log(sum(4, 5))
+
+// type inference
+const isLegal = (age: number) => {
+    return age > 18
+}
+
+const runAfter1s = (func: () => void) => {
+    setTimeout(func, 1000)
+}
+runAfter1s(() => console.log('Hello there!'))

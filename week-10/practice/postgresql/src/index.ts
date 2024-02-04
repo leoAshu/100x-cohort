8f005f36c33bd2ad255641b43713de1c809531ac
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-03 19:49:40 -0800

    week-10: postgres connection and create table

--- week-10/practice/postgresql/src/index.ts
+++ week-10/practice/postgresql/src/index.ts
@@ -1,2 +1,34 @@
 import dotenv from 'dotenv'
 dotenv.config()
+
+import { Client } from 'pg'
+
+const client = new Client({
+    connectionString: process.env.DB_URL
+})
+
+const connectClient = async () => {
+    try {
+        await client.connect()
+        console.log('Connected to DB')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const createUsersTable = async () => {
+    await connectClient()
+
+    const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
+    console.log(result)
+}
+
+createUsersTable()

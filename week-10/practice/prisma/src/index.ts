546ec937a9d61213e013356950030e81b51717a0
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-16 19:08:29 -0800

    week-10: prisma practice

--- week-10/practice/prisma/src/index.ts
+++ week-10/practice/prisma/src/index.ts
@@ -2,17 +2,38 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const insertUser = async (email: string, password: string, firstName: string, lastName: string) => {
-    const user = await prisma.user.create({
+// const insertUser = async (email: string, password: string, firstName: string, lastName: string) => {
+//     const user = await prisma.user.create({
+//         data: {
+//             email,
+//             password,
+//             firstName,
+//             lastName
+//         }
+//     })
+
+//     console.log(user)
+// }
+
+// insertUser('a@b.com', 'password', 'First', 'Last')
+
+interface updateUserParams {
+    firstName: string
+    lastName: string
+}
+
+const updateUser = async (email: string, { firstName, lastName }: updateUserParams) => {
+    const response = await prisma.user.update({
         data: {
-            email,
-            password,
             firstName,
             lastName
+        },
+        where: {
+            email
         }
     })
 
-    console.log(user)
+    console.log(response)
 }
 
-insertUser('a@b.com', 'password', 'First', 'Last')
+updateUser('a@b.com', { firstName: 'FirstName', lastName: 'LastName' })

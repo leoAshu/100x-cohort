ea2a552020c3da0fe1bd5b634615cf2b411e8b7d
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-16 17:20:17 -0800

    week-10: postgres practice

--- week-10/practice/postgresql/src/index.ts
+++ week-10/practice/postgresql/src/index.ts
@@ -16,19 +16,32 @@ const connectClient = async () => {
     }
 }
 
-const createUsersTable = async () => {
-    await connectClient()
+// const createUsersTable = async () => {
+//     await connectClient()
 
-    const result = await client.query(`
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
-    `)
-    console.log(result)
+//     const result = await client.query(`
+//         CREATE TABLE users (
+//             id SERIAL PRIMARY KEY,
+//             username VARCHAR(50) UNIQUE NOT NULL,
+//             email VARCHAR(255) UNIQUE NOT NULL,
+//             password VARCHAR(255) NOT NULL,
+//             created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+//         );
+//     `)
+//     console.log(result)
+// }
+
+// createUsersTable()
+
+const insertData = async () => {
+    await client.connect()
+
+    const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`
+    const values = ['username', 'email', 'password']
+
+    const res = await client.query(insertQuery, values)
+
+    console.log(res)
 }
 
-createUsersTable()
+insertData()

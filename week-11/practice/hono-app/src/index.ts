fa0a2326b986aa9cb6fa77be66f84489a327e674
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-18 18:35:54 -0800

    week-11: hono middlewares

--- week-11/practice/hono-app/src/index.ts
+++ week-11/practice/hono-app/src/index.ts
@@ -1,6 +1,18 @@
-import { Hono } from 'hono'
+import { Hono, MiddlewareHandler } from 'hono'
+import { Env } from 'hono/netlify'
 
 const app = new Hono()
+
+const authMiddleware: MiddlewareHandler<Env, never, {}> = async (c, next) => {
+    if (c.req.header('Authorization')) {
+        // Do validation
+        await next()
+    } else {
+        return c.json({
+            message: 'You do not have access!'
+        })
+    }
+}
 
 app.get('/', (c) => {
     return c.json({
@@ -8,7 +20,7 @@ app.get('/', (c) => {
     })
 })
 
-app.post('/', async (c) => {
+app.post('/', authMiddleware, async (c) => {
     const body = await c.req.json()
     console.log(body)
     console.log(c.req.header('Authorization'))
